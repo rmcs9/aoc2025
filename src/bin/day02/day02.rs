f8345a1854aaf62082c11ba3063d45f2e8f3b82b
ryan <rmcswny@gmail.com>
2025-12-03 00:41:35 -0500

    day02 better comments/readability

--- src/bin/day02/day02.rs
+++ src/bin/day02/day02.rs
@@ -24,6 +24,7 @@ fn part1(data: &Vec<(String, String, u64, u64)>) -> u64 {
     let mut result = 0;
     for range in data {
         let (min_str, max_str, min_num, max_num) = range; 
+        // place difference refers to the difference between the number of digits in the min - max
         let place_difference = max_str.len() - min_str.len();
         
         // if the entire range falls on a numerical length that is odd,
@@ -33,12 +34,16 @@ fn part1(data: &Vec<(String, String, u64, u64)>) -> u64 {
             continue
         }
 
-        // if place difference is 0 only the current range is valid for checks 
+        // if place difference is 0  
         if place_difference == 0 {
             result += find_invalid(&range);
-        } else if place_difference == 1 { // if the place difference is 1, 1 of the 2 ranges is
-                                          // valid for checks
+        } else if place_difference == 1 { // if the place difference is 1, divide the range into
+                                          // pieces at the digit boundry. for example on the range:
+                                          // 991 - 1234, the ranges get divided into 991 - 999 and
+                                          //     1000 - 1234
             let base: u64 = 10;
+            // since numbers with an odd number of digits cant be considered, only do the range
+            // with the even number of digits
             if max_str.len() % 2 == 1 {
                 let adjusted_max_bound = max_num - ((max_num - base.pow((max_str.len() - 1) as u32)) + 1);
                 let range = (min_str.clone(), adjusted_max_bound.to_string(), min_num.clone(), adjusted_max_bound);
@@ -61,11 +66,11 @@ fn part2(data: &Vec<(String, String, u64, u64)>) -> u64 {
         let (min_str, max_str, min_num, max_num) = range; 
         let place_difference = max_str.len() - min_str.len();
 
-        // if place difference is 0 only the current range is valid for checks 
+        // if place difference is 0
         if place_difference == 0 {
             result += find_invalid_2(&range);
-        } else if place_difference == 1 { // if the place difference is 1, 1 of the 2 ranges is
-                                          // valid for checks
+        } else if place_difference == 1 { 
+            // odd ranges can be considered now so split at the digit boundry and try everything
             let base: u64 = 10;
             let adjusted_max_bound = max_num - ((max_num - base.pow((max_str.len() - 1) as u32)) + 1);
             //ummmmmmmm
@@ -84,6 +89,7 @@ fn part2(data: &Vec<(String, String, u64, u64)>) -> u64 {
 fn find_invalid(range: &(String, String, u64, u64)) -> u64 {
     let (min_str, max_str, min_num, max_num) = range; 
     let mut result = 0;
+    // determine the length of the repeated pattern
     let section_length = max_str.len() / 2;
 
     let min_sig_str: &str = &min_str[0..section_length];
@@ -93,6 +99,7 @@ fn find_invalid(range: &(String, String, u64, u64)) -> u64 {
     let max_sig_num: u64 = max_sig_str.parse().unwrap(); 
 
     for i in min_sig_num..max_sig_num + 1 {
+        // repeat the target number twice and check if it fits within the required range
         let invalid_str = format!("{}{}", i.to_string(), i.to_string()); 
         let invalid_num: u64 = invalid_str.parse::<u64>().unwrap();
 
@@ -108,9 +115,13 @@ fn find_invalid_2(range: &(String, String, u64, u64)) -> u64 {
     let mut mem: HashSet<u64> = HashSet::new();
     let mut result = 0;
     let (min_str, max_str, min_num, max_num) = range; 
+    // determine if the digit length is even or odd
     let parity = min_str.len() % 2 == 0; 
 
+    // determine the max possible length of a repeated pattern
     let max_pattern_length = if parity { min_str.len() / 2 } else { min_str.len() / 3 };    
+    // from the size of the max pattern, work down attempting to make smaller and smaller patterns
+    // each time
     for i in (1..max_pattern_length + 1).rev() {
         if min_str.len() % i != 0 {
             continue;
@@ -124,6 +135,8 @@ fn find_invalid_2(range: &(String, String, u64, u64)) -> u64 {
         for j in min_sig_num..max_sig_num + 1 {
 
             let mut invalid_str = String::new(); 
+            // create a string with the repeating pattern enough times to fill the target amount of
+            // digits. 
             for _ in 0..min_str.len() / i {
                 invalid_str.push_str(&j.to_string());
             }
